8d81afa0e078ab7c3c4ac6cb56c04ab639809a84
Swe Hon <85326512+yeoswehon@users.noreply.github.com>
2025-07-13 01:11:23 +0800

    Update mex.cpp

--- Miscellaneous/mex.cpp
+++ Miscellaneous/mex.cpp
@@ -1,9 +1,9 @@
 // Time Complexity: O(n)
 template<typename T>
-T mex(const vector<T>& a,int &n) { // set n = MAX_N
+T mex(vector<T>& a,int &n) {
     vector<bool>seen(n+1);
     for (T &x:a) {
-        if (x>=0&&x<=n) seen[x]|=1;
+        if (x>=0&&x<=n) seen[x]=1;
     }
     for (int i=0;i<=n;i++)
         if (!seen[i]) return i;
