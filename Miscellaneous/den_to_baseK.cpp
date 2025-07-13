2f8635c207c41a00fb3ce7059fc5891840b3ecd1
Swe Hon <85326512+yeoswehon@users.noreply.github.com>
2025-07-14 01:58:39 +0800

    Create den_to_baseK.cpp

---
+++ Miscellaneous/den_to_baseK.cpp
@@ -0,0 +1,20 @@
+// while loop
+template<typename T>
+T to_base_int(T n, int k) {
+    T r=0,p=1;
+    while (n) {
+        r+= p*(n%k);
+        p*=10;
+        n/=k;
+    }
+    return r;
+}
+
+// for loop
+template<typename T>
+T to_base_int(T n, int k) {
+    T r=0,p=1;
+    for (;n;n/= k,p *= 10)
+        r+= p*(n%k);
+    return r;
+}
