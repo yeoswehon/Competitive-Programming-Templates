d9f45c5736cf52de4918e8dbe239974d7d8d9f90
Swe Hon <85326512+yeoswehon@users.noreply.github.com>
2025-07-14 17:58:12 +0800

    Create bipartitecheck.cpp

---
+++ Graph/bipartitecheck.cpp
@@ -0,0 +1,23 @@
+template <typename T>
+bool bipartite(int n,vector<vector<T>> &adj,vi &color) {
+    color.assign(n, -1);
+    queue<int> q;
+    for(int s=0;s<n;s++){
+        if (color[s]!=-1) continue;
+        color[s]=0;
+        q.push(s);
+        while(!q.empty()){
+            auto u =q.front();
+            q.pop();
+            for(T &v:adj[u]){
+                if (color[v]==-1){
+                    color[v]=color[u]^1;
+                    q.push(v);
+                } else if (color[v] == color[u]){
+                    return false;
+                }
+            }
+        }
+    }
+    return true;
+}
