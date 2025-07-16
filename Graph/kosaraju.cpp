bd9b62416466076a55aa33b42f8b2abfe4b21d77
Swe Hon <85326512+yeoswehon@users.noreply.github.com>
2025-07-16 17:23:54 +0800

    Update kosaraju.cpp

--- Graph/kosaraju.cpp
+++ Graph/kosaraju.cpp
@@ -5,18 +5,18 @@ vi ord;
 
 void dfs1(int v){
     vis[v]=1;
-    for (int &u:g[v]) if (!vis[u]) dfs1(u);
+    for(int &u:g[v]) if (!vis[u]) dfs1(u);
     ord.push_back(v);
 }
 
 void dfs2(int v){
     vis[v]=1;
-    for (int &u:gt[v]) if (!vis[u]) dfs2(u);
+    for(int &u:gt[v]) if (!vis[u]) dfs2(u);
 }
 
 int kosaraju(){ // no of SCCs
     vis.assign(N,0);
-    for (int i=0;i<N;i++) if (!vis[i]) dfs1(i);
+    for(int i=0;i<N;i++) if (!vis[i]) dfs1(i);
     vis.assign(N,0);
     int cnt=0;
     for (int i=N-1;i>=0;i--){
