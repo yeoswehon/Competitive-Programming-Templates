092671f8d3b6a70c6692ccccb9f2b1c0523ce84d
Swe Hon <85326512+yeoswehon@users.noreply.github.com>
2025-07-24 15:37:42 +0800

    Create F - LCS.cpp

---
+++ Dynamic Programming/AtCoder Educational DP Contest/F - LCS.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll=long long;
+using dd=long double;
+using vi=vector<int>;
+using vl=vector<long long>;
+using vvi=vector<vector<int>>;
+using pii=pair<int,int>;
+using pll=pair<long long,long long>;
+#define pb push_back
+#define eb emplace_back
+#define ff first
+#define ss second
+#define read(a) for(auto&b:a) cin>>b;
+#define FOR(i,n) for(ll i=0;(i)<(ll)(n);(i)++)
+#define FOR2(i,a,b) for(ll i=(a);i<ll(b);i++)
+#define FOR3(i,n) for(ll i=(n);i>=ll(0);i--)
+#define FOR4(i,a,b) for(ll i=(a);i>=ll(b);i--)
+#define SUM(a) accumulate((a).begin(),(a).end(),0LL)
+#define SORT(a) sort(a.begin(),a.end())
+#define RSORT(a) sort(a.rbegin(),a.rend())
+#define UNIQUE(a) sort(a.begin(),a.end());a.erase(unique(a.begin(),a.end()),a.end()); // sorting included
+#define VEC(a,n) vector<int> a(n);for(auto& b:a) cin>>b;
+#define sz size()
+#define anslol cout<<ans<<"\n"
+#define all(x) x.begin(),x.end()
+#define newline cout<<endl
+#define debug(v) cout<<"Line(" << __LINE__ << ") -> "<<#v<<" = "<<(v)<<"\n";
+template <typename T>void print(const vector<T>&v){for (const T&x:v)cout<<x<<" ";cout<<"\n";}
+template<class T,class S>inline bool chmax(T&a,const S&b){return(a<b?a=b,1:0);}
+template<class T,class S>inline bool chmin(T&a,const S&b){return(a>b?a=b,1:0);}
+
+const ll MAX_N=300000;
+const ll MOD=1000000007;
+const ll INF=1e18;
+
+void solve() {
+    string a,b;
+    cin>>a>>b;
+    int n=a.sz;
+    int m=b.sz;
+    vvi dp(n+1,vi(m+1)); // dp[i][j] = LCS of a[1..i] b[1...j], dp[i][0] = 0, dp[0][j] = 0
+    FOR(i,n){
+        FOR(j,m){
+            dp[i+1][j+1]=max(dp[i+1][j],dp[i][j+1]);
+            if (a[i]==b[j]){
+                chmax(dp[i+1][j+1],dp[i][j]+1);
+            }
+        }
+    }
+    string ans;
+    ll l=n,r=m;
+    while(l&&r){
+        if (a[l-1]==b[r-1]){
+            ans+=a[l-1];
+            l--;
+            r--;
+        } else if (dp[l][r-1]>=dp[l-1][r]) r--; // mismatch, skipping r gives an optimal LCS
+        else l--;
+    }
+    reverse(all(ans));
+    anslol;
+}
+
+int main() {
+    int tc=1;
+    ios::sync_with_stdio(false);cin.tie(nullptr);
+    while (tc--) {
+        solve();
+    }
+    return 0;
+}
