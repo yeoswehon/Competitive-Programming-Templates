e1579f66f67c3c2c59a3412f752ea2f3e7147dec
Swe Hon <85326512+yeoswehon@users.noreply.github.com>
2025-07-24 21:15:36 +0800

    Create J - Sushi.cpp

---
+++ Dynamic Programming/AtCoder Educational DP Contest/J - Sushi.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll=long long;
+using dd=long double;
+using vi=vector<int>;
+using vl=vector<long long>;
+using vvi=vector<vector<int>>;
+using pii=pair<int,int>;
+using pll=pair<long long,long long>;
+#define pb push_back
+#define eb emplace_back
+#define ff first
+#define ss second
+#define read(a) for(auto&b:a) cin>>b;
+#define FOR(i,n) for(ll i=0;(i)<(ll)(n);(i)++)
+#define FOR2(i,a,b) for(ll i=(a);i<ll(b);i++)
+#define FOR3(i,n) for(ll i=(n);i>=ll(0);i--)
+#define FOR4(i,a,b) for(ll i=(a);i>=ll(b);i--)
+#define SUM(a) accumulate((a).begin(),(a).end(),0LL)
+#define SORT(a) sort(a.begin(),a.end())
+#define RSORT(a) sort(a.rbegin(),a.rend())
+#define UNIQUE(a) sort(a.begin(),a.end());a.erase(unique(a.begin(),a.end()),a.end()); // sorting included
+#define VEC(a,n) vector<int> a(n);for(auto& b:a) cin>>b;
+#define sz size()
+#define anslol cout<<ans<<"\n"
+#define all(x) x.begin(),x.end()
+#define newline cout<<endl
+#define debug(v) cout<<"Line(" << __LINE__ << ") -> "<<#v<<" = "<<(v)<<"\n";
+template <typename T>void print(const vector<T>&v){for (const T&x:v)cout<<x<<" ";cout<<"\n";}
+template<class T,class S>inline bool chmax(T&a,const S&b){return(a<b?a=b,1:0);}
+template<class T,class S>inline bool chmin(T&a,const S&b){return(a>b?a=b,1:0);}
+
+const ll MAX_N=302;
+const ll MOD2=1000000007;
+const ll INF=1e18;
+
+dd dp[MAX_N][MAX_N][MAX_N]; // dp[i][j][k] = expected # operations to get from state (i,j,k) to (0,0,0)
+// dp[i][j][k] = 1 + ((n-(i+j+k))/n * dp[i][j][k]) + (i/n * dp[i-1][j][k]) + (j/n * dp[i+1][j-1][k])+ (k/n * dp[i][j+1][k-1])
+
+void solve() {
+    dd n;
+    cin>>n;
+    VEC(a,n);
+    vi f(4);
+    FOR(i,n) f[a[i]]++;
+    dp[0][0][0]=0;
+    FOR2(x,1,n+1){
+        FOR3(i,x){
+            FOR3(j,x-i){
+                ll k=x-i-j; // i + j + (x - i - j) = x
+                dp[i][j][k]+=n;
+                if (i) dp[i][j][k]+=i*dp[i-1][j][k];
+                if (j) dp[i][j][k]+=j*dp[i+1][j-1][k];
+                if (k) dp[i][j][k]+=k*dp[i][j+1][k-1];
+                dp[i][j][k]/=x;
+            }
+        }
+    }
+    cout<<fixed<<setprecision(12)<<dp[f[1]][f[2]][f[3]]<<"\n";
+}
+
+int main() {
+    int tc=1;
+    ios::sync_with_stdio(false);cin.tie(nullptr);
+    while (tc--) {
+        solve();
+    }
+    return 0;
+}
