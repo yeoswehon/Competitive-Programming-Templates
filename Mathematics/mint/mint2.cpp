c8d4c8d1aad7d70940df5a38a5b2c089c9ffa682
Swe Hon <85326512+yeoswehon@users.noreply.github.com>
2025-08-01 17:52:32 +0800

    Create mint2.cpp

---
+++ Mathematics/mint/mint2.cpp
@@ -0,0 +1,41 @@
+struct mint {
+    inline static ll MOD = MOD; // change
+    ll v;
+    static void set_mod(ll mod) { MOD = mod; }
+    mint() : v(0) {}
+    mint(ll x) { v = x % MOD; if(v < 0) v += MOD; }
+    mint pow(ll power) const {
+        mint res(1), base(*this);
+        while(power > 0){
+            if(power & 1) res *= base;
+            base *= base; power >>=1;
+        }
+        return res;
+    }
+    mint inv() const { return this->pow(MOD - 2); }
+    mint& operator+=(const mint& o) { v += o.v; if(v >= MOD) v -= MOD; return *this; }
+    mint& operator-=(const mint& o) { if(v < o.v) v += MOD; v -= o.v; return *this; }
+    mint& operator*=(const mint& o) { v = (__int128(v) * o.v) % MOD; return *this; }
+    mint& operator/=(const mint& o) { return (*this) *= o.inv(); }
+    mint operator-() const { return mint(0) - *this; }
+    friend mint operator+(mint a, const mint& b) { a += b; return a; }
+    friend mint operator-(mint a, const mint& b) { a -= b; return a; }
+    friend mint operator*(mint a, const mint& b) { a *= b; return a; }
+    friend mint operator/(mint a, const mint& b) { a /= b; return a; }
+    friend bool operator==(const mint& a, const mint& b) { return a.v == b.v; }
+    friend bool operator!=(const mint& a, const mint& b) { return a.v != b.v; }
+    friend ostream& operator<<(ostream& os, const mint& x) { return os << x.v; }
+    friend istream& operator>>(istream& is, mint& x) { ll tmp; is >> tmp; x = mint(tmp); return is; }
+    inline static vector<mint> F{1}, I{1};
+    static mint C(int n,int k){
+        if(k<0||k>n) return 0;
+        if((int)F.size()<=n){
+            int m=2*n;
+            F.resize(m+1); I.resize(m+1);
+            F[0]=1; for(int i=1;i<=m;i++) F[i]=F[i-1]*i;
+            I[m]=F[m].inv(); for(int i=m;i>0;i--) I[i-1]=I[i]*i;
+        }
+        return F[n]*I[k]*I[n-k];
+    }
+};
+inline auto C=mint::C;
