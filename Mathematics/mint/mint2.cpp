28d8574380d40fbaf616da8fa86dfae976f8f721
Swe Hon <85326512+yeoswehon@users.noreply.github.com>
2025-08-01 18:02:00 +0800

    Update mint2.cpp

--- Mathematics/mint/mint2.cpp
+++ Mathematics/mint/mint2.cpp
@@ -26,16 +26,17 @@ struct mint {
     friend bool operator!=(const mint& a, const mint& b) { return a.v != b.v; }
     friend ostream& operator<<(ostream& os, const mint& x) { return os << x.v; }
     friend istream& operator>>(istream& is, mint& x) { ll tmp; is >> tmp; x = mint(tmp); return is; }
-    inline static vector<mint> F{1}, I{1};
+    inline static vector<mint>F,I;
+    static void init(int MAX){
+        F.resize(MAX+1);
+        I.resize(MAX+1);
+        F[0]=1;
+        for(int i=1;i<=MAX;i++) F[i]=F[i-1]*i;
+        I[MAX]=F[MAX].inv();
+        for(int i=MAX;i>0;i--) I[i-1]=I[i]*i;
+    }
     static mint C(int n,int k){
-        if(k<0||k>n) return 0;
-        if((int)F.size()<=n){
-            int m=2*n;
-            F.resize(m+1); I.resize(m+1);
-            F[0]=1; for(int i=1;i<=m;i++) F[i]=F[i-1]*i;
-            I[m]=F[m].inv(); for(int i=m;i>0;i--) I[i-1]=I[i]*i;
-        }
+        if(k<0||k>n)return 0;
         return F[n]*I[k]*I[n-k];
     }
 };
-inline auto C=mint::C;
