719b6287ec086d0f44badc912bdd6db3ffd3aba8
Swe Hon <85326512+yeoswehon@users.noreply.github.com>
2025-12-10 01:50:04 +0800

    Update extendedgcd.cpp

--- Mathematics/extendedgcd.cpp
+++ Mathematics/extendedgcd.cpp
@@ -1,8 +1,6 @@
 // Returns x0 and y0 such that a*x0 + b*y0 = gcd(a,b)
-template<typename T>pair<T,T>egcd(T a,T b){
-    if(!b)return{1,0};
-    auto[x1,y1]=egcd(b,a%b);
-    T x=y1;
-    T y=x1-a/b*y1;
-    return{x,y};
-}
+auto egcd=[&](auto &&self, ll a,ll b, ll &x0, ll &y0) -> ll{
+    if (!b) return x0=1,y0=0,a;
+    ll d=self(self,b,a%b,y0,x0);
+    return y0-=(a/b)*x0,d;
+};
