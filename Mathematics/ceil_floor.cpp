3e927da72c1aa004ede5d0a84ddbd048da9c2f13
Swe Hon <85326512+yeoswehon@users.noreply.github.com>
2025-12-10 01:41:27 +0800

    Update and rename floordiv.cpp to ceil_floor.cpp

---
+++ Mathematics/ceil_floor.cpp
@@ -0,0 +1,10 @@
+auto floorbaby=[&](ll a, ll b) -> ll {
+    ll q=a/b;
+    ll r=a%b;
+    if (r&&((a<0)^(b<0))) q--;
+    return q;
+};
+
+auto ceilbaby=[&](ll a, ll b) -> ll {
+    return -floorbaby(-a,b);
+};
--- Mathematics/floordiv.cpp
+++
@@ -1,6 +0,0 @@
-auto f=[&](ll a, ll b) -> ll {
-    ll q=a/b;
-    ll r=a%b;
-    if (r&&((a<0)^(b<0))) q--;
-    return q;
-};
