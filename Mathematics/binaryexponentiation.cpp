35b684f43ef81328fa50a7cdba4454e9e202a222
Swe Hon <85326512+yeoswehon@users.noreply.github.com>
2026-01-20 21:43:54 +0800

    Update binaryexponentiation.cpp

--- Mathematics/binaryexponentiation.cpp
+++ Mathematics/binaryexponentiation.cpp
@@ -1,28 +1,28 @@
 ll binpow_rec(ll a, ll b) { // recursive
-    if (b == 0) return 1;
-    ll r = binpow_rec(a, b >> 1);
-    r = r * r;
-    if (b & 1) r *= a;
+    if (b==0) return 1;
+    ll r=binpow_rec(a,b >>1);
+    r*=r;
+    if (b&1) r*=a;
     return r;
 }
 
 ll binpow(ll a, ll b) { // iterative
-    ll res = 1;
-    while (b > 0) {
-        if (b & 1) res = res * a;
-        a = a * a;
-        b >>= 1;
+    ll res=1;
+    while (b>0) {
+        if (b&1) res*=a;
+        a*=a;
+        b>>=1;
     }
     return res;
 }
 
 ll modpow(ll a, ll b, ll m = MOD) { // iterative with mod
-    ll res = 1;
-    a %= m;
-    while (b > 0) {
-        if (b & 1) res = (res * a) % m;
-        a = (a * a) % m;
-        b >>= 1;
+    ll res=1;
+    a%=m;
+    while (b>0) {
+        if (b&1) res=(res*a)%m;
+        a=(a*a)%m;
+        b>>=1;
     }
     return res;
 }
